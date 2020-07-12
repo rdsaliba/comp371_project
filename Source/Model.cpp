ad19c18fa3b3596cc0df6260851297bbd5e0fc02
Hau <haugillesche@gmail.com>
2020-07-12 13:57:41 -0400

    +Implemented Model class

---
+++ Source/Model.cpp
@@ -0,0 +1,19 @@
+#include "Model.h"
+
+Model::Model() {
+	this->position = vec3(0.0f, 0.0f, 0.0f);
+	this->scaling = 0.0f;
+	this->rotation = vec3(0.0f, 0.0f, 0.0f);
+}
+
+Model::Model(vec3 position, float scaling) {
+	this->position = position;
+	this->scaling = scaling;
+	this->rotation = vec3(0.0f, 0.0f, 0.0f);
+}
+
+void Model::updatePosition(vec3 moveVector) {
+	this->position.x += moveVector.x;
+	this->position.y += moveVector.y;
+	this->position.z += moveVector.z;
+}
---
+++ Source/Model.h
@@ -0,0 +1,41 @@
+#ifndef MODEL_H
+#define MODEL_H
+#include <glm/glm.hpp>
+using namespace glm;
+/// <summary>
+/// Movable 3D model location/transformation information
+/// @author Hau Gilles Che 
+/// </summary>
+class Model {
+private:
+	//Location of model in 3D world
+	vec3 position;
+	//Scaling value applied onto model to change base size
+	float scaling;
+	//Orientation of model in 3D world
+	vec3 rotation;
+
+public:
+	Model();
+	Model(vec3 position, float size);
+
+	vec3 getPosition() { return position; }
+	float getScaling() { return scaling; }
+	vec3 getRotation() { return rotation; }
+
+	void setPosition(vec3 position) { this->position = position; }
+	void setSize(float scaling) { this->scaling = scaling; }
+	void setRotation(vec3 rotation) { this->rotation = rotation; }
+
+	void updatePosition(vec3 moveVector);
+	void updateScaling(float value) { this->scaling += value; }
+
+	//Translation values
+	void x(float value) { this->position.x += value; }
+	void y(float value) { this->position.y += value; }
+	void z(float value) { this->position.z += value; }
+
+	void updateRotationY(float yValue) { this->rotation.y += yValue; }
+};
+
+#endif // !MODEL_H
