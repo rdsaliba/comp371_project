18fa17ac21b1acf6c7b8feba31518fc7ba30c47b
Hau <haugillesche@gmail.com>
2020-07-18 18:41:03 -0400

    +Added/updated some comments

--- Source/ModelUtilities.h
+++ Source/ModelUtilities.h
@@ -6,11 +6,11 @@
 using namespace glm;
 namespace ModelUtilities{
 	//Constants for size standardization across the different models
-	vec3 longComponentScalingVector();
-	vec3 shortComponentScalingVector();
-	vec3 medComponentScalingVector();
+	vec3 longComponentScalingVector(); //5 units wide
+	vec3 shortComponentScalingVector(); //3 units wide
+	vec3 medComponentScalingVector(); //4 units wide
 
-	/// Enum associated to segments building a Letter model
+	///Segment positions for model building
 	enum class ComponentType {
 		TOP,
 		LEFT,
@@ -19,6 +19,7 @@ namespace ModelUtilities{
 		CENTER
 	};
 
+	//Size of desired component to generate for model building
 	enum class ComponentSize {
 		SHORT,
 		LONG,
@@ -33,8 +34,9 @@ namespace ModelUtilities{
 	mat4 createComponent(ComponentSize size);
 	mat4 scaleComponent(mat4 component, vec3 scalingVector);
 	mat4 rotateComponent(mat4 component, float angle, vec3 rotationVector);
-	mat4 rotateComponentOnZ(mat4 component);
-	mat4 rotateComponentOnY(mat4 component);
+	mat4 rotateComponentOnZ(mat4 component); //rotate component 90 degrees along the Z axis
+	mat4 rotateComponentOnY(mat4 component); //rotate component 90 degrees along the Y axis
 	vec3 getComponentPosition(mat4 component, ComponentType componentType, ComponentSize componentSize, ModelType modelType);
+
 };
 
